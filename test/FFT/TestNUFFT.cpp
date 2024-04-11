d62806de83f055fcbdbfa5a38e273a2bff54a683
Ryan Ammann <rammann@ethz.ch>
2024-04-11 10:53:16 +0200

    had to add openmp dependency to CMakeLists of test/FFT/

--- test/FFT/TestNUFFT.cpp
+++ test/FFT/TestNUFFT.cpp
@@ -63,9 +63,10 @@ struct generate_random {
 
 int main(int argc, char* argv[]){
 
-    
+     /*
     ippl::initialize(argc, argv);
     {
+       
         constexpr unsigned int dim = 3;
         using Mesh_t               = ippl::UniformCartesian<double, dim>;
         using Centering_t          = Mesh_t::DefaultCentering;
@@ -92,108 +93,114 @@ int main(int argc, char* argv[]){
         2.0 * pi / double(pt[0]),
         2.0 * pi / double(pt[1]),
         2.0 * pi / double(pt[2]),
-    };
+        };
 
-    //std::array<double, dim> dx = {
-    //    25.0 / double(pt[0]),
-    //    25.0 / double(pt[1]),
-    //    25.0 / double(pt[2]),
-    //};
-    typedef ippl::Vector<double, 3> Vector_t;
+        //std::array<double, dim> dx = {
+        //    25.0 / double(pt[0]),
+        //    25.0 / double(pt[1]),
+        //    25.0 / double(pt[2]),
+        //};
+        typedef ippl::Vector<double, 3> Vector_t;
 
-    Vector_t hx = {dx[0], dx[1], dx[2]};
-    Vector_t origin = {-pi, -pi, -pi};
-    //Vector_t origin = {0, 0, 0};
-    ippl::UniformCartesian<double, 3> mesh(owned, hx, origin);
+        Vector_t hx = {dx[0], dx[1], dx[2]};
+        Vector_t origin = {-pi, -pi, -pi};
+        //Vector_t origin = {0, 0, 0};
+        ippl::UniformCartesian<double, 3> mesh(owned, hx, origin);
 
-    playout_type pl(layout, mesh);
+        playout_type pl(layout, mesh);
 
-    bunch_type bunch(pl);
-    bunch.setParticleBC(ippl::BC::PERIODIC);
-   
-    using size_type = ippl::detail::size_type;
-
-
-    size_type Np = std::pow(512,3) * 5;
+        bunch_type bunch(pl);
+        bunch.setParticleBC(ippl::BC::PERIODIC);
     
-    typedef ippl::Field<Kokkos::complex<double>, dim, Mesh_t, Centering_t> field_type;
+        using size_type = ippl::detail::size_type;
 
-    field_type field(mesh, layout);
-    field_type field_dft(mesh, layout);
 
-    ippl::ParameterList fftParams;
+        size_type Np = std::pow(512,3) * 5;
+        
+        typedef ippl::Field<Kokkos::complex<double>, dim, Mesh_t, Centering_t> field_type;
 
-    typedef ippl::NUFFT<3,double,Mesh_t, Centering_t> NUFFT_type;
+        field_type field(mesh, layout);
+        field_type field_dft(mesh, layout);
 
-    std::unique_ptr<NUFFT_type> nufft;
-    int type = 1;
+        ippl::ParameterList fftParams;
+
+
+        //finufft_default_opts(NULL);
+        
+        typedef ippl::NUFFT<3,double,Mesh_t, Centering_t> NUFFT_type;
+
+        //std::unique_ptr<NUFFT_type> nufft;
+        int type = 1;
+        
+        
+        
+        Vector_t minU = {-pi, -pi, -pi};
+        Vector_t maxU = {pi, pi, pi};
+
+        size_type nloc = Np/ippl::Comm->size();
+
+        bunch.create(nloc);
+        
+        nufft = std::make_unique<NUFFT_type>(layout, nloc, type, fftParams);
+        
+        Kokkos::Random_XorShift64_Pool<> rand_pool64((size_type)(42));
+        Kokkos::parallel_for(nloc,
+                            generate_random<Vector_t, Kokkos::Random_XorShift64_Pool<>, dim>(
+                            bunch.R.getView(), bunch.Q.getView(), rand_pool64, minU, maxU));
+        
+        
+        nufft->transform(bunch.R, bunch.Q, field);
+        
+        auto field_result = Kokkos::create_mirror_view_and_copy(
+                            Kokkos::HostSpace(), field.getView());
+
+        ippl::Vector<int, 3> kVec;
+        kVec[0] = (int)(0.37 * pt[0]);
+        kVec[1] = (int)(0.26 * pt[1]);
+        kVec[2] = (int)(0.13 * pt[2]);
+
+        const int nghost = field.getNghost();
+
+        int iInd = (pt[0]/2 + kVec[0] + nghost);
+        int jInd = (pt[1]/2 + kVec[1] + nghost);
+        int kInd = (pt[2]/2 + kVec[2] + nghost);
+
+
+        Kokkos::complex<double> reducedValue(0.0, 0.0);
+
+        auto Rview = bunch.R.getView();
+        auto Qview = bunch.Q.getView();
+
+        Kokkos::complex<double> imag = {0.0, 1.0};
+
+        Kokkos::parallel_reduce("NUDFT type1", nloc,
+                                KOKKOS_LAMBDA(const size_t idx, Kokkos::complex<double>& valL) {
+
+                                    double arg = 0.0;
+                                    for(size_t d = 0; d < dim; ++d) {
+                                        arg += kVec[d]*Rview(idx)[d];
+                                    }
+
+                                    valL += (Kokkos::cos(arg) 
+                                    - imag * Kokkos::sin(arg)) * Qview(idx);
+                                }, Kokkos::Sum<Kokkos::complex<double>>(reducedValue));
+        
+        double abs_error_real = std::fabs(reducedValue.real() - field_result(iInd, jInd, kInd).real());
+        double rel_error_real = std::fabs(reducedValue.real() - field_result(iInd, jInd, kInd).real()) /std::fabs(reducedValue.real());
+        double abs_error_imag = std::fabs(reducedValue.imag() - field_result(iInd, jInd, kInd).imag());
+        double rel_error_imag = std::fabs(reducedValue.imag() - field_result(iInd, jInd, kInd).imag()) /std::fabs(reducedValue.imag());
     
+        std::cout << "Abs Error in real part: " << std::setprecision(16) 
+                << abs_error_real << " Rel. error in real part: " << std::setprecision(16) << rel_error_real << std::endl;
+        std::cout << "Abs Error in imag part: " << std::setprecision(16) 
+                << abs_error_imag << " Rel. error in imag part: " << std::setprecision(16) << rel_error_imag << std::endl;
+        std::cout << "Field result: " << std::setprecision(16) 
+                << field_result(iInd,jInd,kInd).real() << " " << std::setprecision(16) << field_result(iInd,jInd,kInd).imag() 
+                << "index: " << iInd << "," << jInd << "," << kInd << std::endl;
 
-    Vector_t minU = {-pi, -pi, -pi};
-    Vector_t maxU = {pi, pi, pi};
-
-    size_type nloc = Np/ippl::Comm->size();
-
-    bunch.create(nloc);
-    nufft = std::make_unique<NUFFT_type>(layout, nloc, type, fftParams);
-    Kokkos::Random_XorShift64_Pool<> rand_pool64((size_type)(42));
-    Kokkos::parallel_for(nloc,
-                         generate_random<Vector_t, Kokkos::Random_XorShift64_Pool<>, dim>(
-                         bunch.R.getView(), bunch.Q.getView(), rand_pool64, minU, maxU));
-    
-
-    nufft->transform(bunch.R, bunch.Q, field);
-    
-    auto field_result = Kokkos::create_mirror_view_and_copy(
-                        Kokkos::HostSpace(), field.getView());
-
-    ippl::Vector<int, 3> kVec;
-    kVec[0] = (int)(0.37 * pt[0]);
-    kVec[1] = (int)(0.26 * pt[1]);
-    kVec[2] = (int)(0.13 * pt[2]);
-
-    const int nghost = field.getNghost();
-
-    int iInd = (pt[0]/2 + kVec[0] + nghost);
-    int jInd = (pt[1]/2 + kVec[1] + nghost);
-    int kInd = (pt[2]/2 + kVec[2] + nghost);
-
-
-    Kokkos::complex<double> reducedValue(0.0, 0.0);
-
-    auto Rview = bunch.R.getView();
-    auto Qview = bunch.Q.getView();
-
-    Kokkos::complex<double> imag = {0.0, 1.0};
-
-    Kokkos::parallel_reduce("NUDFT type1", nloc,
-                             KOKKOS_LAMBDA(const size_t idx, Kokkos::complex<double>& valL) {
-
-                                double arg = 0.0;
-                                for(size_t d = 0; d < dim; ++d) {
-                                    arg += kVec[d]*Rview(idx)[d];
-                                }
-
-                                valL += (Kokkos::cos(arg) 
-                                - imag * Kokkos::sin(arg)) * Qview(idx);
-                            }, Kokkos::Sum<Kokkos::complex<double>>(reducedValue));
-    
-    double abs_error_real = std::fabs(reducedValue.real() - field_result(iInd, jInd, kInd).real());
-    double rel_error_real = std::fabs(reducedValue.real() - field_result(iInd, jInd, kInd).real()) /std::fabs(reducedValue.real());
-    double abs_error_imag = std::fabs(reducedValue.imag() - field_result(iInd, jInd, kInd).imag());
-    double rel_error_imag = std::fabs(reducedValue.imag() - field_result(iInd, jInd, kInd).imag()) /std::fabs(reducedValue.imag());
- 
-    std::cout << "Abs Error in real part: " << std::setprecision(16) 
-              << abs_error_real << " Rel. error in real part: " << std::setprecision(16) << rel_error_real << std::endl;
-    std::cout << "Abs Error in imag part: " << std::setprecision(16) 
-              << abs_error_imag << " Rel. error in imag part: " << std::setprecision(16) << rel_error_imag << std::endl;
-    std::cout << "Field result: " << std::setprecision(16) 
-              << field_result(iInd,jInd,kInd).real() << " " << std::setprecision(16) << field_result(iInd,jInd,kInd).imag() 
-              << "index: " << iInd << "," << jInd << "," << kInd << std::endl;
-
-
+        
     }
     ippl::finalize();
-
+*/
     return 0;
 }
