79c4da6f4030b2db2f3a4f1b455d000be70c9bb6
Ryan Ammann <rammann@ethz.ch>
2024-05-06 10:23:45 +0200

    Added more nodes

--- src/OrthoTree/TreeOpenPoissonSolver.h
+++ src/OrthoTree/TreeOpenPoissonSolver.h
@@ -95,7 +95,7 @@ namespace ippl
         void Farfield(){
             
             // Number of Fourier nodes as defined in (3.36)
-            int nf = static_cast<int>(Kokkos::ceil(4 * Kokkos::log(1/eps_m)));
+            int nf = static_cast<int>(Kokkos::ceil(4 * Kokkos::log(1/eps_m))) * 2;
             constexpr unsigned int dim = 3;
 
 
@@ -188,7 +188,7 @@ namespace ippl
         void DifferenceKernel(){
 
             const unsigned int dim = 3; 
-            int nf = static_cast<int>(Kokkos::ceil(6 / Kokkos::numbers::pi * Kokkos::log(1/eps_m))) * 3;
+            int nf = static_cast<int>(Kokkos::ceil(6 / Kokkos::numbers::pi * Kokkos::log(1/eps_m))) * 4;
 
             // Iterate through levels of the tree
             for(unsigned int depth=0; depth < tree_m.GetMaxDepth(); ++depth){
