9fd24df08a7f69ebd337aba75a629c72eacc1780
amazzetta <aaron.mazzetta@gmail.com>
2024-10-28 21:16:31 +0100

    smol restructur

--- src/OrthoTree/OrthoTree.h
+++ src/OrthoTree/OrthoTree.h
@@ -20,43 +20,73 @@ namespace ippl {
     class OrthoTree {
         using real_coordinate = real_coordinate_template<Dim>;
         using grid_coordinate = grid_coordinate_template<Dim>;
-        using particle_type = particle_type_template<Dim>;
-        using bounds_type = BoundingBox<Dim>;
+        using particle_t = particle_type_template<Dim>;
+        using bounds_t = BoundingBox<Dim>;
+
+        using aid_list_t = Kokkos::vector<Kokkos::pair<morton_code, size_t>>;
 
         const size_t max_depth_m;
         const size_t max_particles_per_node_m;
-        const bounds_type root_bounds_m;
+        const bounds_t root_bounds_m;
         const Morton<Dim> morton_helper;
 
-        Kokkos::vector<morton_code> tree;
+        Kokkos::vector<morton_code> tree; // this is just a placeholder rn
+
+        // probably best kept as member, need to compare with new particle codes for update
+        aid_list_t aid_list;
 
     public:
 
-        OrthoTree(size_t max_depth, size_t max_particles_per_node, const bounds_type& root_bounds)
+        OrthoTree(size_t max_depth, size_t max_particles_per_node, const bounds_t& root_bounds)
             : max_depth_m(max_depth), max_particles_per_node_m(max_particles_per_node),
             root_bounds_m(root_bounds), morton_helper(max_depth)
         { }
 
-        void build_tree_naive_sequential(particle_type const& particles)
+        void build_tree_naive_sequential(particle_t const& particles)
         {
+            // insert the root into the tree
+            tree.push_back(morton_code(0)); // maybe store morton_code(0) s.t. we can call morton::root_val or smth
+
+            initialize_aid_list(particles);
+
+            std::cout << "aid_list size: " << aid_list.size() << std::endl;
+            for ( const auto& [morton, id] : aid_list ) {
+                std::cout << "(morton=0b" << std::bitset<16>(morton) << ", id=" << id << ")\n";
+            }
+        }
+
+        /**
+         * @brief returns a list of morton_codes (nodes) / particle ids
+         * this is intended for testing
+         */
+        Kokkos::vector<Kokkos::pair<morton_code, Kokkos::vector<size_t>> get_orthotree() const;
+
+        // todo @aaron, generic tree walker that takes 2 funcs (select & apply)
+        void traverse_tree();
+
+    private:
+
+        /**
+         * @brief initializes the aid list which has form vec{pair{morton_code, size_t}}
+         * will sort the aidlist in ascending morton codes
+         *
+         * @param particles
+         */
+        void initialize_aid_list(particle_t const& particles)
+        {
+            // maybe get getGlobalNum()?
             const size_t n_particles = particles.getLocalNum();
             const size_t nodes_per_edge = (size_t(1) << max_depth_m);
 
-            // insert the root into the tree
-            tree.push_back(morton_code(0));
+            // store dimensions of root bounding box
+            const real_coordinate root_bounds_size = root_bounds_m.get_max() - root_bounds_m.get_min();
 
-            std::vector<std::pair<morton_code, size_t>> aid_list(n_particles);
+            aid_list.resize(n_particles);
+
             for ( size_t i = 0; i < n_particles; ++i ) {
-                grid_coordinate grid_coord;
-
-                for ( size_t j = 0; j < Dim; ++j ) {
-                    const double dist = particles.R(i)[j] - root_bounds_m.get_min()[j];
-                    const double grid_len = root_bounds_m.get_max()[j] - root_bounds_m.get_min()[j];
-
-                    const double ratio = dist / grid_len;
-                    grid_coord[j] = static_cast<grid_t>(nodes_per_edge * ratio);
-                }
-
+                // real/grid_coordinate has lazy eval
+                const real_coordiante normalized = (particles.R(i) - root_bounds_m.get_min()) / root_bounds_size;
+                const grid_coordinate grid_coord = static_cast<grid_coordinate>(normalized * nodes_per_edge);
                 aid_list[i] = { morton_helper.encode(grid_coord, max_depth_m), i };
             }
 
@@ -64,12 +94,7 @@ namespace ippl {
             {
                 return a.first < b.first;
             });
-
-            for ( const auto& [morton, id] : aid_list ) {
-                std::cout << "(morton=0b" << std::bitset<16>(morton) << ", id=" << id << ")\n";
-            }
         }
-
     };
 
 } // namespace ippl
