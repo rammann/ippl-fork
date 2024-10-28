c013f5daefe6c1878de2c4c8f7f9df9e18996e08
amazzetta <aaron.mazzetta@gmail.com>
2024-10-28 18:32:02 +0100

    basic structure for naive build function

--- src/OrthoTree/OrthoTree.h
+++ src/OrthoTree/OrthoTree.h
@@ -6,6 +6,8 @@
 #include "MortonHelper.h"
 #include "BoundingBox.h"
 
+#include <Kokkos_Vector.hpp>
+#include <Kokkos_Pair.hpp>
 namespace ippl {
 
     // this is defined outside of Types.h on purpose, as this is likely to change in the finalised implementation
@@ -23,18 +25,41 @@ namespace ippl {
         const size_t max_particles_per_node;
         const bounds_type root_bounds_m;
         const bounds_type rasterizer_m;
+        const Morton morton_helper;
+
+        Kokkos::vector<morton_code> tree;
 
     public:
 
         explicit OrthoTree(size_t max_depth, size_t max_particles_per_node, const bounds_type& root_bounds)
             : max_depth_m(max_depth), max_particles_per_node_m(max_particles_per_node),
-            root_bounds_m(root_bounds)
+            root_bounds_m(root_bounds), morton_helper(max_depth)
         {
             const size_t leaf_nodes_per_edge = (size_t(1) << max_depth);
             rasterizer_m = root_bounds_m.get_raster(leaf_nodes_per_edge);
         }
 
-        void build_tree_naive_sequential(particle_type const& particles);
+        void build_tree_naive_sequential(particle_type const& particles)
+        {
+            const size_t n_particles = particles.size();
+
+            // insert the root into the tree
+            tree.push_back(morton_code(0));
+
+            Kokkos::vector<Kokkos::pair<morton_code, size_t>> aid_list(n_particles);
+            for ( size_t i = 0; i < n_particles; ++i ) {
+                aid_list[i] = { morton_helper.encode(particles.R(i), rasterizer_m, max_depth_m), i };
+            }
+
+            std::sort(aid_list.begin(), aid_list.end(), [ ] (const auto& a, const auto& b)
+            {
+                return a.second < b.second;
+            });
+
+            for ( const auto& [morton, id] : aid_list ) {
+                std::cout << "(morton=" << morton << ", id=" << id << ")\n";
+            }
+        }
 
     };
 
