a842cd0a60d630bcb103ed0a8c704aeb2f908af2
amazzetta <aaron.mazzetta@gmail.com>
2024-10-29 19:02:49 +0100

    finalized mvp for bounds, extracted implementation to .hpp file

--- src/OrthoTree/BoundingBox.h
+++ src/OrthoTree/BoundingBox.h
@@ -1,20 +1,35 @@
 #ifndef ORTHO_TREE_BOUNDS_GUARD
 #define ORTHO_TREE_BOUNDS_GUARD
 
-#include <sstream> // for to_string()
 #include "Types.h"
+
 namespace ippl {
 
+    /**
+     * @brief This class manages bounds of our domain. As i understand it we only care about the root bounds.
+     * Most of this class is hence not used, but its nice to have.
+     *
+     * Can also be used to convert a grid coordinate back to a real valued BoundingBox.
+     *
+     */
     template <size_t Dim>
     class BoundingBox {
         using real_coordinate = real_coordinate_template<Dim>;
         using grid_coordinate = real_coordinate_template<Dim>;
 
-        real_coordinate min_m;
-        real_coordinate max_m;
+        const real_coordinate min_m;
+        const real_coordinate max_m;
 
     public:
+
+        /**
+         * @brief Construct a new BoundingBox with coordinates {0},{0} in R^Dim
+         */
         BoundingBox() = default;
+
+        /**
+         * @brief Construct a new BoundingBox with fixed coordinates
+         */
         BoundingBox(real_coordinate min, real_coordinate max)
             : min_m(min), max_m(max)
         { }
@@ -22,63 +37,44 @@ namespace ippl {
         real_coordinate get_min() const { return min_m; }
         real_coordinate get_max() const { return max_m; }
 
-        BoundingBox get_raster(size_t raster_size) const
-        {
-            BoundingBox raster;
-            raster.min_m = min_m;
-            for ( size_t i = 0; i < Dim; ++i ) {
-                raster.max_m[i] = raster.min_m[i] + (max_m[i] - min_m[i]) / raster_size;
-            }
+        /**
+         * @brief Calculates the raster of this bounding box based on a raster size
+         *
+         * @param raster_size = 2^max_depth
+         * @return BoundingBox
+         */
+        BoundingBox get_raster(size_t raster_size) const;
 
-            return raster;
-        }
-
-        real_coordinate get_center() const
-        {
-            real_coordinate center;
-            for ( size_t i = 0; i < Dim; ++i ) {
-                center[i] = min_m[i] + (max_m[i] - min_m[i]) / 2;
-            }
-            return center;
-        }
+        /**
+         * @brief Get the center of this bounding box
+         *
+         * @return real_coordinate center of thix box
+         */
+        real_coordinate get_center() const;
 
         /**
          * @brief prints the bounding box to a string of the form: {(min),(max)}
          *
          * @return std::string
          */
-        std::string to_string() const
-        {
-            // stringstream because ippl::Vector supports this
-            std::stringstream res;
-            res << "{min=" << min_m << ", max=" << max_m << "}";
-            return res.str();
-        }
+        std::string to_string() const;
 
-        static BoundingBox bounds_from_grid_coord(const BoundingBox& root_bounds, const grid_coordinate& grid_coord, const size_t depth, const size_t max_depth)
-        {
-            BoundingBox result;
-
-            // Total nodes per edge at the lowest level (leaf level)
-            const double nodes_per_edge_max_depth = static_cast<double>(1 << max_depth);
-
-            // Scaling factor to move from the finest level to the current depth
-            const double nodes_per_edge_cur_depth = static_cast<double>(1 << depth);
-
-            // Calculate the size of each node's edge at this depth level
-            real_coordinate node_size = (root_bounds.max_m - root_bounds.min_m) / nodes_per_edge_max_depth;
-
-            // Compute the minimum corner of the bounding box for this grid coordinate at the current depth
-            result.min_m = root_bounds.min_m + (static_cast<real_coordinate>(grid_coord) * node_size);
-
-            // The maximum corner is the minimum corner plus the node size
-            result.max_m = result.min_m + (node_size * (nodes_per_edge_max_depth / nodes_per_edge_cur_depth));
-
-            return result;
-        }
+        /**
+         * @brief Calculate the real valued bounding box based on a coordinate (anchor) in the grid.
+         * This is not the final version.
+         *
+         * @param root_bounds The root bounding box
+         * @param grid_coord The anchor of the node we want to convert
+         * @param depth The depth of the node we want to convert
+         * @param max_depth The max depth of our tree
+         * @return BoundingBox
+         */
+        static BoundingBox bounds_from_grid_coord(const BoundingBox& root_bounds, const grid_coordinate& grid_coord, const size_t depth, const size_t max_depth);
 
     };
 
 } // namespace ippl
+
+#include "BoundingBox.hpp"
 
 #endif // ORTHO_TREE_BOUNDS_GUARD
---
+++ src/OrthoTree/BoundingBox.hpp
@@ -0,0 +1,51 @@
+#include "BoundingBox.h"
+
+#include <sstream> // for to_string()
+
+namespace ippl {
+
+    template <size_t Dim>
+    BoundingBox<Dim> BoundingBox<Dim>::get_raster(size_t raster_size) const
+    {
+        BoundingBox raster;
+        raster.min_m = min_m;
+        for ( size_t i = 0; i < Dim; ++i ) {
+            raster.max_m[i] = raster.min_m[i] + (max_m[i] - min_m[i]) / raster_size;
+        }
+
+        return raster;
+    }
+
+    template <size_t Dim>
+    BoundingBox<Dim>::real_coordinate BoundingBox<Dim>::get_center() const
+    {
+        real_coordinate center;
+        for ( size_t i = 0; i < Dim; ++i ) {
+            center[i] = min_m[i] + (max_m[i] - min_m[i]) / 2;
+        }
+        return center;
+    }
+
+    template <size_t Dim>
+    std::string BoundingBox<Dim>::to_string() const
+    {
+        // stringstream because ippl::Vector supports this and im lazy
+        std::stringstream res;
+        res << "{min=" << min_m << ", max=" << max_m << "}";
+        return res.str();
+    }
+
+    template <size_t Dim>
+    BoundingBox<Dim> BoundingBox<Dim>::bounds_from_grid_coord(const BoundingBox<Dim>& root_bounds, const BoundingBox<Dim>::grid_coordinate& grid_coord, const size_t depth, const size_t max_depth)
+    {
+        BoundingBox result;
+        const double nodes_per_edge_max_depth = static_cast<double>(1 << max_depth);
+        const double nodes_per_edge_cur_depth = static_cast<double>(1 << depth);
+        real_coordinate node_size = (root_bounds.max_m - root_bounds.min_m) / nodes_per_edge_max_depth;
+        result.min_m = root_bounds.min_m + (static_cast<real_coordinate>(grid_coord) * node_size);
+        result.max_m = result.min_m + (node_size * (nodes_per_edge_max_depth / nodes_per_edge_cur_depth));
+
+        return result;
+    }
+
+} // namespace ippl
