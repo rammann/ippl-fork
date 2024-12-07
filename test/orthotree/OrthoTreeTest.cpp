6174d3bc2ed396db4323c9f7b5aafe90a81f8268
amazzetta <aaron.mazzetta@gmail.com>
2024-12-07 22:00:37 +0100

    added new particle distribution for more interesting plots

--- test/orthotree/OrthoTreeTest.cpp
+++ test/orthotree/OrthoTreeTest.cpp
@@ -15,8 +15,8 @@ int main(int argc, char* argv[])
     {
         static constexpr size_t Dim = 2;
         const size_t max_particles  = 10;
-        const size_t max_depth      = 5;
-        const size_t num_particles  = 1000;  // per processor
+        const size_t max_depth      = 8;
+        const size_t num_particles  = 5000;  // per processor
 
         const auto MIN_BOUND = 0.0;
         const auto MAX_BOUND = 1.0;
@@ -42,19 +42,48 @@ int main(int argc, char* argv[])
         }
 
         typename bunch_type::particle_position_type::HostMirror R_host = bunch.R.getHostMirror();
+
+        // RANDOM INITIALISATION
         // typename bunch_type::rho_container_type::HostMirror RHO_host   =
         // bunch.rho.getHostMirror();
+        /*
         for (unsigned int i = 0; i < num_particles; ++i) {
             // R_host(i) = ippl::Vector<double, Dim>{unif(eng), unif(eng), unif(eng)};
             R_host(i) = ippl::Vector<double, Dim>{unif(eng), unif(eng)};
             //  RHO_host(i) = 0;
+        }
+        */
+
+        // THIS GENERATES A SPIRAL DISTRIBUTION
+        double armCount     = 2.0;   // Number of spiral arms
+        double armTightness = -0.1;  // Tightness of the spiral arms
+        double center_x     = (MAX_BOUND - MIN_BOUND) / 2;
+        double center_y     = (MAX_BOUND - MIN_BOUND) / 2;
+        double max_distance = 0.9 * (MAX_BOUND - MIN_BOUND) / 2;
+        for (unsigned i = 0; i < num_particles; ++i) {
+            double angle    = static_cast<double>(rand()) / RAND_MAX * 2.0 * M_PI;
+            double distance = (static_cast<double>(rand()) / RAND_MAX) * max_distance;
+
+            double totalArmAngle = 5.0;
+
+            for (int j = 0; j < armCount; ++j) {
+                // add spiral arm effect to the angle
+                double armAngle =
+                    armTightness * angle + (j + 1) * distance / max_distance * 2.0 * M_PI;
+                double x = center_x + distance * cos(armAngle + totalArmAngle);
+                double y = center_y + distance * sin(armAngle + totalArmAngle);
+
+                R_host(i) = {x, y};
+                totalArmAngle += armAngle;
+            }
         }
 
         Kokkos::deep_copy(bunch.R.getView(), R_host);
         // Kokkos::deep_copy(bunch.rho.getView(), RHO_host);
 
         bunch.update();
-        tree.build_tree(bunch);
+        // tree.build_tree(bunch);
+        tree.build_tree_naive(bunch);
 
         // stuff 1
         // tree.build_tree_naive(particles);
