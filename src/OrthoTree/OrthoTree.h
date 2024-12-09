abc48d205cda7f0d0baeacda887f0e49b0aeae6f
Caspar Schucan <caspar.schucan@gmail.com>
2024-12-09 11:37:27 +0100

    working refactor of partition algo

--- src/OrthoTree/OrthoTree.h
+++ src/OrthoTree/OrthoTree.h
@@ -129,6 +129,8 @@ namespace ippl {
          */
         Kokkos::vector<morton_code> partition(Kokkos::vector<morton_code>& octants,
                                               Kokkos::vector<size_t>& weights);
+        Kokkos::View<morton_code*> partition(Kokkos::View<morton_code*> octants,
+                                             Kokkos::View<size_t*> weights);
 
         /**
          * ALGO 8
@@ -158,6 +160,8 @@ namespace ippl {
 #pragma endregion  // balancing
 
 #pragma region helpers
+
+        
         /**
          * @brief Compares the following aspects of the trees:
          * - n_particles
