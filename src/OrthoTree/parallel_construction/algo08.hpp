a316f5c0e372d6e675d58a5c2ad4cb253dae176c
qstaro <a@b.c>
2024-12-15 13:02:43 +0100

    alg8 compiles

--- src/OrthoTree/parallel_construction/algo08.hpp
+++ src/OrthoTree/parallel_construction/algo08.hpp
@@ -17,21 +17,24 @@ namespace ippl {
 
 namespace ippl {
     template <size_t Dim>
-    Kokkos::vector<morton_code> OrthoTree<Dim>::linearise_octants(
-        const Kokkos::vector<morton_code>& octants) {
+    Kokkos::View<morton_code*> OrthoTree<Dim>::linearise_octants(
+        const Kokkos::View<morton_code*>& octants) {
         START_FUNC;
         logger << "size: " << octants.size() << endl;
-        Kokkos::vector<morton_code> linearised;
+        Kokkos::View<morton_code*> linearised("linearised", octants.size());
 
+        size_t j = 0;
         for (size_t i = 0; i < octants.size() - 1; ++i) {
             if (morton_helper.is_ancestor(octants[i + 1], octants[i])) {
                 continue;
             }
 
-            linearised.push_back(octants[i]);
+            linearised[j] = octants[i];
+            ++j;
         }
 
-        linearised.push_back(octants.back());
+        linearised[j] = octants[octants.size() - 1];
+        Kokkos::resize(linearised, j+1);
 
         logger << "finished, size is: " << linearised.size() << endl;
         END_FUNC;
