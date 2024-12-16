60fbdba2607f153631946fdb51c2558f94f72b9d
saarora <a@b.c>
2024-12-16 15:26:13 +0100

    algo8 test

--- unit_tests/OrthoTree/parallel_construction/algo8.cpp
+++ unit_tests/OrthoTree/parallel_construction/algo8.cpp
@@ -3,40 +3,42 @@
 
 using namespace ippl;
 
-// TEST(LineariseTestOct, LineariseOctantsTest) {
-//     static constexpr size_t Dim = 3;
-//     const size_t max_depth = 3;
-//     Morton<Dim> morton(max_depth);
-//     OrthoTree<Dim> tree(max_depth, 10, BoundingBox<Dim>({0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}));
-//     Kokkos::vector<morton_code> octs;
-//     octs.push_back(morton.encode({0, 0, 0}, 2));
-//     octs.push_back(morton.encode({0, 4, 0}, 1));
-//     octs.push_back(morton.encode({0, 6, 6}, 2));
-//     Kokkos::vector<morton_code> expected = {};
+TEST(LineariseTestOct, LineariseOctantsTest) {
+    static constexpr size_t Dim = 3;
+    const size_t max_depth = 3;
+    Morton<Dim> morton(max_depth);
+    OrthoTree<Dim> tree(max_depth, 10, BoundingBox<Dim>({0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}));
+    Kokkos::View<morton_code*> octs("octs", 3*9);
+    octs(0) = morton.encode({0, 0, 0}, 2);
+    octs(1) = morton.encode({0, 4, 0}, 1);
+    octs(2) = morton.encode({0, 6, 6}, 2);
+    Kokkos::vector<morton_code> expected = {};
 
-//     size_t n_parents = octs.size();
-//     for(size_t i = 0; i < n_parents; ++i)
-//     {
-//         vector_t<morton_code> children = morton.get_children(octs[i]);
-//         for(size_t j = 0; j < 8; ++j)
-//         {
-//             octs.push_back(children[j]);
-//             expected.push_back(children[j]);
-//         }
-//     }
+    size_t n_parents = 3;
+    size_t idx = 3;
+    for(size_t i = 0; i < n_parents; ++i)
+    {
+        vector_t<morton_code> children = morton.get_children(octs[i]);
+        for(size_t j = 0; j < 8; ++j)
+        {
+            octs(idx) = children[j];
+            idx++;
+            expected.push_back(children[j]);
+        }
+    }
 
-//     std::cerr << "I MAANGED TO GET TILL HERE\n";
+    std::cerr << "I MAANGED TO GET TILL HERE" << std::endl;
 
-//     // octants need to be sorted
-//     std::sort(expected.begin(), expected.end());
-//     std::sort(octs.begin(), octs.end());
+    // octants need to be sorted
+    std::sort(expected.begin(), expected.end());
+    std::sort(octs.data(), octs.data() + octs.size());
 
-//     Kokkos::vector<morton_code> linearised = tree.linearise_octants(octs);
-//     EXPECT_EQ(linearised.size(), expected.size());
-//     for (size_t i = 0; i < linearised.size(); ++i) {
-//         EXPECT_EQ(linearised[i], expected[i]);
-//     }
-// }
+    Kokkos::View<morton_code*> linearised = tree.linearise_octants(octs);
+    EXPECT_EQ(linearised.size(), expected.size());
+    for (size_t i = 0; i < linearised.size(); ++i) {
+        EXPECT_EQ(linearised[i], expected[i]);
+    }
+}
 
 int main(int argc, char** argv) {
     // Initialize MPI and IPPL
