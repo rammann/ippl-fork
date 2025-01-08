6dbc323857a3db966a4a96363ef49dba71390b0e
mmathieu <mmathieu@ethz.ch>
2025-01-08 14:04:22 +0100

    Changed test for parallel_construction4

--- unit_tests/OrthoTree/parallel_construction/full_test/4_ranks.cpp
+++ unit_tests/OrthoTree/parallel_construction/full_test/4_ranks.cpp
@@ -1,8 +1,10 @@
 #include <Kokkos_Core.hpp>
 
 #include <Kokkos_Sort.hpp>
+#include <ctime>
 #include <ostream>
 #include <random>
+#include <string>
 
 #include "OrthoTree/OrthoTreeTypes.h"
 
@@ -23,7 +25,70 @@ TEST(ParallelConstruction4, AssertWorldSize) {
 }
 
 TEST(ParallelConstruction4, ActualTest) {
+    /*
     runTests();
+    */
+    const size_t num_particles = 6;
+    const size_t max_depth = 4;
+    const size_t max_particles = 3;
+    const double min_bound = 0.0;
+    const double max_bound = 1.0;
+    static constexpr size_t Dim = 2;
+
+    typedef ippl::ParticleSpatialLayout<double, Dim> playout_type;
+    typedef ippl::OrthoTreeParticle<playout_type> bunch_type;
+
+    playout_type PLayout;
+    bunch_type bunch(PLayout);
+
+    bunch.create(num_particles);
+
+    typename bunch_type::particle_position_type::HostMirror R_host = bunch.R.getHostMirror();
+    if(Comm->rank() == 0){
+        R_host(0) = ippl::Vector<double, Dim>{0.170, 0.080};
+        R_host(1) = ippl::Vector<double, Dim>{0.171, 0.080};
+        R_host(2) = ippl::Vector<double, Dim>{0.172, 0.080};
+        R_host(3) = ippl::Vector<double, Dim>{0.173, 0.080};
+        R_host(4) = ippl::Vector<double, Dim>{0.440, 0.440};
+        R_host(5) = ippl::Vector<double, Dim>{0.441, 0.440};
+    } else if(Comm->rank() == 1){
+        R_host(0) = ippl::Vector<double, Dim>{0.442, 0.440};
+        R_host(1) = ippl::Vector<double, Dim>{0.443, 0.440};
+        R_host(2) = ippl::Vector<double, Dim>{0.960, 0.080};
+        R_host(3) = ippl::Vector<double, Dim>{0.961, 0.080};
+        R_host(4) = ippl::Vector<double, Dim>{0.962, 0.080};
+        R_host(5) = ippl::Vector<double, Dim>{0.963, 0.080};
+    } else if(Comm->rank() == 2){
+        R_host(0) = ippl::Vector<double, Dim>{0.810, 0.300};
+        R_host(1) = ippl::Vector<double, Dim>{0.811, 0.300};
+        R_host(2) = ippl::Vector<double, Dim>{0.960, 0.300};
+        R_host(3) = ippl::Vector<double, Dim>{0.961, 0.300};
+        R_host(4) = ippl::Vector<double, Dim>{0.810, 0.700};
+        R_host(5) = ippl::Vector<double, Dim>{0.811, 0.700};
+    } else if(Comm->rank() == 3){
+        R_host(0) = ippl::Vector<double, Dim>{0.812, 0.700};
+        R_host(1) = ippl::Vector<double, Dim>{0.813, 0.700};
+        R_host(2) = ippl::Vector<double, Dim>{0.960, 0.960};
+        R_host(3) = ippl::Vector<double, Dim>{0.961, 0.960};
+        R_host(4) = ippl::Vector<double, Dim>{0.962, 0.960};
+        R_host(5) = ippl::Vector<double, Dim>{0.963, 0.960};
+    }
+
+    Kokkos::deep_copy(bunch.R.getView(), R_host);
+    bunch.update();
+
+    BoundingBox<Dim> root_bounds({min_bound, min_bound}, {max_bound, max_bound});
+    OrthoTree<Dim> tree(max_depth, max_particles, root_bounds);
+    Kokkos::View<morton_code*> parallel_tree =  tree.build_tree(bunch);
+
+    std::string log_str = "Rank " + std::to_string(Comm->rank()) + ": parallel_tree = {";
+    for(size_t i = 0; i < parallel_tree.size(); i++){
+        log_str += std::to_string(parallel_tree(i));
+        if(i != parallel_tree.size()-1) log_str += ", ";
+    }
+    log_str += "}\n";
+    std::cerr << log_str;
+
 }
 
 // this is required to test the orthotree, as it depends on ippl
